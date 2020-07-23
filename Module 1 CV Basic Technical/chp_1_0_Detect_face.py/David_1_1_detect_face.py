0a797eca86f088c148684b948d8b36cdd12f9a52
wcsodw1 <wcsodw1@gmail.com>
2020-07-23 17:15:47 +0800

    20200723 Result_Image_Update
    20200723 Result_Image_Update

--- Module 1 CV Basic Technical/chp_1_0_Detect_face.py/David_1_1_detect_face.py
+++ Module 1 CV Basic Technical/chp_1_0_Detect_face.py/David_1_1_detect_face.py
@@ -35,6 +35,7 @@ cv2.waitKey(0)
 
     # 3.1  load the face detector 
 detector = cv2.CascadeClassifier("../../../detector/haarcascade.xml")
+
 print(detector) 
     # 3.2 detect faces in the image
 rects = detector.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 9,
@@ -55,4 +56,4 @@ cv2.imshow("Faces", gray)
 cv2.waitKey(0) # Visualize the image until手動把它關閉
     # imwrite :  Save the converted image
 cv2.imwrite("../../data/imwrite/chp1_1/basketball_box.jpg", image) 
-cv2.imwrite("../../data/imwrite/chp1_1/basketball_gray_box.jpg", gray) 
+cv2.imwrite("../../data/imwrite/chp1_1/basketball_gray_box.jpg", gray)
